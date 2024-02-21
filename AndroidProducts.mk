0f5347e4eb992ed32509e9fa9b65e303cffc710c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-02-21 12:39:46 +0100

    gta9p: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-X216B Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_gta9p.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_gta9p-user \
+    omni_gta9p-userdebug \
+    omni_gta9p-eng
